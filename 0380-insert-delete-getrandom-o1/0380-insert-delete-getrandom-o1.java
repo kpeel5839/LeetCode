402802fa3bf0a13cd3171c626945e4fe6a818d43
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2022-11-29 16:33:25 +0900

    Time: 74 ms (29.31%), Space: 114.2 MB (13.81%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,24 +1,37 @@
 import java.util.*;
 
 class RandomizedSet {
-    Set<Integer> values;
+    Map<Integer, Integer> valuesToIndex;
+    List<Integer> values;
 
     public RandomizedSet() {
-        values = new HashSet<>();
+        valuesToIndex = new HashMap<>();
+        values = new ArrayList<>();
     }
     
     public boolean insert(int val) {
-        if (values.contains(val)) { // already exists
-            return false;
+        // insert 하면 containsKey 로 확인
+        if (valuesToIndex.containsKey(val)) {
+            return false;   
         }
 
+        // valuesToIndex 에다가 list.size() - 1 을 넣어서 인덱싱해준다.
         values.add(val);
+        valuesToIndex.put(val, values.size() - 1);
         return true;
     }
     
     public boolean remove(int val) {
-        if (values.contains(val)) {
-            values.remove(val);
+        if (valuesToIndex.containsKey(val)) {
+            int indexOfValue = valuesToIndex.get(val); // value in list
+            // 마지막 인덱스의 값을 indexOfValue 에다가 옮겨야함
+            // 그리고 마지막 값을 indexOfValue 에다가 옮겨야 함
+            // 그리고 list 에서 remove 연산을 해야함, 마지막 놈을        
+            valuesToIndex.put(values.get(values.size() - 1), indexOfValue);
+            values.set(indexOfValue, values.get(values.size() - 1));
+            valuesToIndex.remove(val);
+            values.remove(values.size() - 1);
+
             return true;
         }
 
@@ -27,15 +40,7 @@ class RandomizedSet {
     
     public int getRandom() {
         Random random = new Random();
-        int extractIndex = random.nextInt(Math.min(5, values.size()));
-        int randomNumber = 0;
-        Iterator<Integer> it = values.iterator();
-        
-        for (int i = 0; i <= extractIndex; i++) {
-            randomNumber = it.next();
-        }
-
-        return randomNumber;   
+        return values.get(random.nextInt(values.size()));
     }
 }
 
