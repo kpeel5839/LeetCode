f4dda5bc96a71804717dc9b86f62fe5cab24426d
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-01-07 21:43:26 +0900

    Time: 3 ms (66.98%), Space: 62.7 MB (68.29%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -4,14 +4,9 @@ class Solution {
         int start = 0;
         int index = 0;    
         int nowGasAmount = 0;
-        int[] totalCost = new int[size]; 
-        
-        for (int i = 0; i < gas.length; i++) {
-            totalCost[i] = gas[i] - cost[i];
-        }
         
         while (start < size) {
-            nowGasAmount += totalCost[index % size];
+            nowGasAmount += (gas[index % size] - cost[index % size]);
             
             if (nowGasAmount >= 0) {
                 if ((start + size - 1) == index) { // 완전 다 돈 경우
