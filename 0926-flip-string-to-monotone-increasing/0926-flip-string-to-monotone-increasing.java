dfd1b816265ab5be36bfab95ff8427c53eac2dcc
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-01-17 11:34:36 +0900

    Time: 18 ms (45.42%), Space: 43 MB (65.76%) - LeetHub

---
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int oppose(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        
+        return 0;
+    }
+    
+    public int minFlipsMonoIncr(String s) {
+        int[][] flipCount = new int[2][s.length()];
+        int allRemoveCount = 0;
+        
+        int lastChar = s.charAt(s.length() - 1) - '0';
+        if (lastChar == 1) {
+            allRemoveCount++;
+        }
+        
+        flipCount[oppose(lastChar)][s.length() - 1] = 1;
+        flipCount[lastChar][s.length() - 1] = 0;
+        
+        for (int i = s.length() - 2; i > -1; i--) {
+            int now = s.charAt(i) - '0';
+            
+            if (now == 1) {
+                allRemoveCount++;
+            }
+            
+            if (i == s.length() - 2) {
+                flipCount[oppose(now)][i] += flipCount[1][i + 1] + 1;
+                flipCount[now][i] += flipCount[1][i + 1];
+            }
+            
+            if (i != s.length() - 2 && now == 0) {
+                flipCount[0][i] += Math.min(flipCount[0][i + 1], flipCount[1][i + 1]);
+                flipCount[1][i] += flipCount[1][i + 1] + 1;
+            }
+            
+            if (i != s.length() - 2 && now == 1) {
+                flipCount[0][i] += Math.min(flipCount[0][i + 1], flipCount[1][i + 1]) + 1;
+                flipCount[1][i] += flipCount[1][i + 1];
+            }        
+        }
+        
+        int result = Math.min(allRemoveCount, Math.min(flipCount[0][0], flipCount[1][0]));
+        return result;
+    }
+}
