f8a2e6a14121f2e5f0537fb87ff08e75d150f32e
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-01-17 15:42:02 +0900

    Time: 11 ms (77.97%), Space: 43 MB (65.76%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -1,51 +1,22 @@
 class Solution {
-    public int oppose(int num) {
-        if (num == 0) {
-            return 1;
-        }
-        
-        return 0;
-    }
-    
-    public int isFlip(int pivot, int compare) {
-        if (pivot == compare) {
-            return 0;
-        }
-        
-        return 1;
-    }
     
     public int minFlipsMonoIncr(String s) {
-        int[][] flipCount = new int[2][s.length()];
-        int allRemoveCount = 0;
+        int ones = 0;
+        int answer = 0;
         
-        int lastChar = s.charAt(s.length() - 1) - '0';
-        if (lastChar == 1) {
-            allRemoveCount++;
-        }
-        
-        flipCount[oppose(lastChar)][s.length() - 1] = 1;
-        flipCount[lastChar][s.length() - 1] = 0;
-        
-        for (int i = s.length() - 2; i > -1; i--) {
+        for (int i= 0; i < s.length(); i++) {
             int now = s.charAt(i) - '0';
             
             if (now == 1) {
-                allRemoveCount++;
+                ones++;
             }
             
-            if (i == s.length() - 2) {
-                flipCount[oppose(now)][i] += flipCount[1][i + 1] + 1;
-                flipCount[now][i] += flipCount[1][i + 1];
+            if (now == 0 && ones != 0) {
+                ones--;
+                answer++;
             }
-        
-            if (i != s.length() - 2) {
-                flipCount[0][i] += Math.min(flipCount[0][i + 1], flipCount[1][i + 1]) + isFlip(now, 0);
-                flipCount[1][i] += flipCount[1][i + 1] + isFlip(now, 1);   
-            }     
         }
         
-        int result = Math.min(allRemoveCount, Math.min(flipCount[0][0], flipCount[1][0]));
-        return result;
+        return answer;
     }
 }
