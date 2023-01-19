1500db8fc20e0802fce442003a66943c73b521c3
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-01-19 11:56:05 +0900

    Time: 4 ms (82.72%), Space: 47.7 MB (67.90%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,34 +1,28 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int[] maxSum = new int[nums.length];
-        int[] minSum = new int[nums.length];
         int totalSum = nums[0];
-        maxSum[0] = nums[0];
-        minSum[0] = nums[0];
-        int max = maxSum[0];
-        int min = minSum[0];
+        int maxSum = nums[0];
+        int minSum = nums[0];
+        int max = maxSum;
+        int min = minSum;
         
         for (int i = 1; i < nums.length; i++) {
             totalSum += nums[i];
             
-            if (maxSum[i - 1] < 0) {
-                maxSum[i] = nums[i];
+            if (maxSum <= 0) {
+                maxSum = nums[i];
+            } else {
+                maxSum += nums[i];
             }
             
-            if (maxSum[i - 1] >= 0) {
-                maxSum[i] += maxSum[i - 1] + nums[i];
+            if (minSum < 0) {
+                minSum += nums[i];
+            } else {
+                minSum = nums[i];
             }
             
-            if (minSum[i - 1] > 0) {
-                minSum[i] = nums[i];
-            } 
-            
-            if (minSum[i - 1] <= 0) {
-                minSum[i] = minSum[i - 1] + nums[i];    
-            }
-            
-            min = Math.min(min, minSum[i]);
-            max = Math.max(max, maxSum[i]);
+            min = Math.min(min, minSum);
+            max = Math.max(max, maxSum);
         }
         
         if (totalSum == min) {
