a9ba66ee576048e6c2231a30fbf9e70f50797e44
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-03-04 23:24:38 +0900

    Time: 48 ms (5.26%), Space: 61 MB (17.96%) - LeetHub

---
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
@@ -0,0 +1,45 @@
+class Solution {
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        Set<Integer> set = new HashSet<>();
+        int pivot = minK == maxK ? 1 : 2;
+        Map<Integer, Integer> convert = new HashMap<>();
+        Map<Integer, Integer> index = new HashMap<>();
+        
+        convert.put(minK, maxK);
+        convert.put(maxK, minK);
+        int outRange = -1;
+        long answer = 0;
+        int max = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (minK == nums[i]) {
+                set.add(nums[i]);
+                index.put(minK, i);
+            }
+            
+            if (maxK == nums[i]) {
+                set.add(nums[i]);
+                index.put(maxK, i);
+            }
+            
+            if (minK <= nums[i] && nums[i] <= maxK) {
+                if (set.size() == pivot) {
+                    if (minK == nums[i] || maxK == nums[i]) {
+                        int reverse = convert.get(nums[i]);
+                        max = Math.max(max, index.get(reverse) - outRange);
+                        answer += index.get(reverse) - outRange;
+                    } else {
+                        answer += max;
+                    }
+                }
+            } else {
+                set.clear();
+                index.clear();
+                outRange = i;
+                max = 0;
+            }
+        }
+        
+        return answer;
+    }
+}
