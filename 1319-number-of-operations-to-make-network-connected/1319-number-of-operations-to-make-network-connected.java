2bcbfa8d044556756ac1650005bbd0c4995454c2
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-03-23 09:21:52 +0900

    Time: 8 ms (68.31%), Space: 55.7 MB (91.65%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,43 @@
+class Solution {
+    int[] parent;
+    
+    int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        } 
+        
+        return parent[a] = find(parent[a]);
+    }
+    
+    void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        
+        parent[b] = a;
+    }
+    public int makeConnected(int n, int[][] connections) {
+        parent = new int[n];
+        
+        if (connections.length < (n - 1)) {
+            return -1;
+        }
+    
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+        }
+        
+        for (int i = 0; i < connections.length; i++) {
+            int a = find(connections[i][0]);
+            int b = find(connections[i][1]);
+            
+            union(a, b);
+        }
+        
+        Set<Integer> p = new HashSet<>();
+        for (int i = 0; i < parent.length; i++) {
+            p.add(find(parent[i]));
+        }
+        
+        return p.size() - 1;
+    }
+}
