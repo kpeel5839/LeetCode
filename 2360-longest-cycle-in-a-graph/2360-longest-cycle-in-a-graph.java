106cb2bfcc14aaced03c33605590634b9a8a49b1
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-03-26 11:24:48 +0900

    Time: 189 ms (22.78%), Space: 88.3 MB (33.20%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -0,0 +1,56 @@
+class Solution {
+    int[] g;
+    int[] id;
+    int idValue = 0;
+    boolean[] scc;
+    Stack<Integer> s = new Stack<>();
+    int ans = -1;
+    
+    public int dfs(int v) {
+        id[v] = idValue++;
+        int min = id[v];
+        s.add(v);
+        
+        if (g[v] != -1 && id[g[v]] == -1) {
+            min = Math.min(min, dfs(g[v]));
+        } else if (g[v] != -1 && !scc[g[v]]) {
+            min = Math.min(min, id[g[v]]);
+        }
+        
+        if (min == id[v]) {        
+            int count = 0;
+            
+            while (true) {
+                int vv = s.pop();
+                scc[vv] = true;
+                count++;
+                
+                if (id[vv] == id[v]) {
+                    break;
+                }
+            }
+            
+            if (count != 1) {
+                ans = Math.max(ans, count);   
+            }            
+        }
+        
+        return min;
+    }
+    
+    public int longestCycle(int[] edges) {
+        g = edges;
+        id = new int[g.length];
+        scc = new boolean[g.length];
+        Arrays.fill(id, -1);
+        
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == -1) {
+                dfs(i);
+            }
+        }
+        
+        System.out.println(Arrays.toString(id));
+        return ans;
+    }
+}
