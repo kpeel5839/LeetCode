8c611977fce802d32cec7c89859b082c766972b2
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-03-30 16:23:45 +0900

    Time: 104 ms (20.17%), Space: 43.6 MB (16.07%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.java
@@ -0,0 +1,46 @@
+class Solution {
+    public Map<String, Boolean> m = new HashMap<>();
+    
+    public boolean isScramble(String s1, String s2) {
+        int n = s1.length();
+        
+        if (n != s2.length()) {
+            return false;
+        }
+        
+        if (s1.equals(s2)) {
+            return true;
+        }
+        
+        if (n == 1) {
+            return false;
+        }
+        
+        String key = s1 + " " + s2;
+        
+        if (m.containsKey(key)) {
+            return m.get(key);
+        }
+        
+        for (int i = 1; i < n; i++) {
+            boolean a = isScramble(s1.substring(0, i), s2.substring(0, i)) 
+                && isScramble(s1.substring(i), s2.substring(i));
+            
+            if (a) {
+                m.put(key, true);
+                return true;
+            }
+            
+            boolean b = isScramble(s1.substring(0, i), s2.substring(n - i))
+                && isScramble(s1.substring(i), s2.substring(0, n - i));
+            
+            if (b) {
+                m.put(key, true);
+                return true;
+            }
+        }
+        
+        m.put(key, false);
+        return false;
+    }
+}
