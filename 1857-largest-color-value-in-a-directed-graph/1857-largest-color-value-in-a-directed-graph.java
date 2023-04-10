48c6817145210f7f92813ee749e1ae6c1784327a
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-04-10 12:04:50 +0900

    Time: 112 ms (43.75%), Space: 102.7 MB (84.82%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.java
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.java
@@ -1,24 +1,69 @@
-class Solution:
-  def largestPathValue(self, colors, edges):
-      n, k = len(colors), 26
-      indegrees = [0] * n
-      graph = [[] for _ in range(n)]
-      for u, v in edges:
-          graph[u].append(v)
-          indegrees[v] += 1
-      zero_indegree = set(i for i in range(n) if indegrees[i] == 0)
-      counts = [[0] * k for _ in range(n)]
-      for i, c in enumerate(colors):
-          counts[i][ord(c) - ord('a')] += 1
-      max_count, visited = 0, 0
-      while zero_indegree:
-          u = zero_indegree.pop()
-          visited += 1
-          for v in graph[u]:
-              for i in range(k):
-                  counts[v][i] = max(counts[v][i], counts[u][i] + (ord(colors[v]) - ord('a') == i))
-              indegrees[v] -= 1
-              if indegrees[v] == 0:
-                  zero_indegree.add(v)
-          max_count = max(max_count, max(counts[u]))
-      return max_count if visited == n else -1
+class Solution {
+    public List<ArrayList<Integer>> graph = new ArrayList<>();
+    public Map<Integer, int[]> m = new HashMap<>();
+    public int[] except = new int[26];
+    public boolean[] v;
+    public String color = null;
+    public boolean existCycle = false;
+    
+    public int[] dfs(int c) {
+        if (m.containsKey(c)) {
+            return m.get(c);
+        }
+
+        if (v[c]) {
+            return except;
+        }
+        
+        v[c] = true;
+        int[] now = new int[26];
+        
+        for (Integer next : graph.get(c)) {                      
+            int[] nextValue = dfs(next);            
+            if (nextValue[0] != -1) { 
+                for (int i = 0; i < 26; i++) {
+                    now[i] = Math.max(now[i], nextValue[i]);
+                }
+            } else {
+                existCycle = true;
+            }
+        }
+        
+        if (existCycle) {
+            Arrays.fill(now, -1);
+        } else {
+            now[color.charAt(c) - 'a']++;
+        }
+        
+        m.put(c, now);
+        return now;
+    }
+    
+    public int largestPathValue(String colors, int[][] edges) {
+        color = colors;
+        v = new boolean[colors.length()];
+        Arrays.fill(except, -1);
+        
+        for (int i = 0; i < colors.length(); i++) {
+            graph.add(new ArrayList<>());    
+        }
+        
+        for (int i = 0; i < edges.length; i++) {
+            graph.get(edges[i][0]).add(edges[i][1]);
+        }
+        
+        int ans = -1;
+        
+        for (int i = 0; i < colors.length(); i++) {
+            if (!v[i]) {            
+                int[] vv = dfs(i);            
+                
+                for (int j = 0; j < vv.length; j++) {
+                    ans = Math.max(ans, vv[j]);
+                }
+            }
+        }
+        
+        return existCycle ? -1 : ans;
+    }
+}
