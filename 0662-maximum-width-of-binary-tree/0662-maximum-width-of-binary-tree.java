0f7769c5f7e5ddb2a8bd928860533df84947bdf9
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-04-20 11:08:39 +0900

    Time: 2 ms (41.32%), Space: 42.2 MB (58.03%) - LeetHub

---
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public Map<Integer, Long> m = new HashMap<>();
+    public long ans = 0;
+    
+    public void dfs(TreeNode c, int d, long n) {
+        if (c == null) {
+            return;
+        }
+        
+        if (!m.containsKey(d)) {
+            m.put(d, n);
+        }
+    
+        ans = Math.max(ans, (n - m.get(d) + 1));
+        dfs(c.left, d + 1, n * 2);
+        dfs(c.right, d + 1, n * 2 + 1);        
+    }
+    
+    public int widthOfBinaryTree(TreeNode root) {
+        dfs(root, 1, 1);
+        return (int) ans;
+    }
+}
