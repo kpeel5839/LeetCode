ec3c193dd6964f8919cf383a994831f8f8138832
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-05-01 11:30:00 +0900

    Time: 45 ms (17.81%), Space: 109.6 MB (16.44%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
@@ -1,63 +1,71 @@
-class DSU {
-public:
-    vector<int> parent, rank;
-    DSU(int n){
-        parent.resize(n, 0);
-        rank.resize(n, 0);
-        
-        for(int i=0;i<n;i++) parent[i] = i;
-    }
-    
-    int Find(int x){
-        return parent[x] = parent[x] == x ? x : Find(parent[x]);
-    }
-    
-    bool Union(int x, int y){
-        int xset = Find(x), yset = Find(y);
-        if(xset != yset){
-            rank[xset] < rank[yset] ? parent[xset] = yset : parent[yset] = xset;
-            rank[xset] += rank[xset] == rank[yset];
-            return true;
+class Solution {    
+    public int find(int x, int[] p) {
+        if (p[x] == x) {
+            return x;
         }
-        return false;
+        
+        return p[x] = find(p[x], p);
     }
-};
-
-class Solution {
-public:
-    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        sort(edges.begin(), edges.end(), [&](auto const &a, auto const &b){
-            return a[0] > b[0];
-        });
-
-        DSU dsu_alice(n+1);
-        DSU dsu_bob(n+1);
-
-        int removed_edge = 0, alice_edges=0, bob_edges=0;
-        for(auto edge : edges){
-            if(edge[0] == 3){
-                if(dsu_alice.Union(edge[1], edge[2])){ // Both Alice & Bob
-                    dsu_bob.Union(edge[1], edge[2]);
-                    alice_edges++;
-                    bob_edges++;
-                }else{
-                    removed_edge++;
+    
+    public void union(int a, int b, int[] p) {
+        p[b] = a;
+    }
+    
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+        int[] a = new int[n + 1];
+        int[] b = new int[n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            a[i] = i;
+            b[i] = i;
+        }
+        
+        Arrays.sort(edges, (o1, o2) -> o2[0] - o1[0]);
+        int count = 0;
+        
+        for (int i = 0; i < edges.length; i++) {
+            int type = edges[i][0];
+            
+            if (type == 3) { // all
+                int a1 = find(edges[i][1], a);
+                int a2 = find(edges[i][2], a);
+                
+                if (a1 != a2) { // 같은 집합이 아니라면, 추가
+                    count++;
+                    union(a1, a2, a);
+                    union(find(edges[i][1], b), find(edges[i][2], b), b);
+                }            
+            } else if (type == 2) { // a
+                int a1 = find(edges[i][1], a);
+                int a2 = find(edges[i][2], a);
+                
+                if (a1 != a2) {
+                    count++;
+                    union(a1, a2, a);
                 }
-            }else if(edge[0] == 2){ //Only Bob
-                if(dsu_bob.Union(edge[1], edge[2])){
-                    bob_edges++;
-                }else{
-                    removed_edge++;
-                }
-            }else{ // Only Alice
-                if(dsu_alice.Union(edge[1], edge[2])){
-                    alice_edges++;
-                }else{
-                    removed_edge++;
+            } else { // b
+                int b1 = find(edges[i][1], b);
+                int b2 = find(edges[i][2], b);
+                
+                if (b1 != b2) {
+                    count++;
+                    union(b1, b2, b);
                 }
             }
         }
+        
+        Set<Integer> aSet = new HashSet<>();
+        Set<Integer> bSet = new HashSet<>();
 
-        return (bob_edges == n - 1 && alice_edges == n - 1) ? removed_edge : -1;
+        for (int i = 1; i <= n; i++) {
+            aSet.add(find(i, a));
+            bSet.add(find(i, b)); 
+        }
+
+        if (aSet.size() != 1 || bSet.size() != 1) {
+            return -1;
+        }
+
+        return edges.length - count;
     }
-};
+}
