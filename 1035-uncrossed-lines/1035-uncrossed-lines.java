5118a88adf493559f750d0ad03e3e246181214d4
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-05-11 23:09:14 +0900

    Time: 5 ms (92.56%), Space: 42.5 MB (30.02%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -1,54 +1,16 @@
 class Solution {
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        Map<Integer, List<Integer>> m = new HashMap<>();
-        int[][] dp = new int[nums1.length][nums2.length];
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
         
         for (int i = 0; i < nums1.length; i++) {
-            if (!m.containsKey(nums1[i])) {
-                m.put(nums1[i], new ArrayList<>());
-            }
-
-            m.get(nums1[i]).add(i);
-        }
-
-        int ans = 0;    
-        for (int i = 0; i < nums2.length; i++) {
-            if (!m.containsKey(nums2[i])) {
-                continue;
-            }        
-
-            for (Integer ii : m.get(nums2[i])) {
-                int max = 0;
-                for (int j = i - 1; 0 <= j; j--) {
-                    if (!m.containsKey(nums2[j])) {
-                        continue;
-                    }
-                
-                    List<Integer> list = m.get(nums2[j]);                
-                    int l = 0;
-                    int r = list.size() - 1;
-                    int idx = -1;
-                    while (l <= r) {
-                        int mid = (l + r) / 2;
-                        
-                        if (list.get(mid) < ii) {
-                            l = mid + 1;
-                            idx = mid;
-                        } else {                        
-                            r = mid - 1;
-                        }
-                    }                    
-                    
-                    if (idx != -1) {                        
-                        max = Math.max(max, dp[list.get(idx)][j]);
-                    }
+            for (int j = 0; j < nums2.length; j++) {
+                dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                if (nums1[i] == nums2[j]) {
+                    dp[i + 1][j + 1] = Math.max(dp[i + 1][j + 1], dp[i][j] + 1);
                 }
-
-                dp[ii][i] = max + 1;
-                ans = Math.max(ans, dp[ii][i]);
             }
         }
-
-        return ans;
+        
+        return dp[nums1.length][nums2.length];
     }
 }
