e5e29a8f7b852d2bd118b3264f5795b0166efef3
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-05-20 19:02:03 +0900

    Time: 11 ms (6.15%), Space: 41.3 MB (44.00%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -0,0 +1,99 @@
+class Solution {
+    public Map<String, ArrayList<Object[]>> g;
+    public Map<String, Double> v;
+    public Map<String, String> p;
+    
+    public String find(String a) {
+        if (p.get(a).equals(a)) {
+            return a;
+        }
+        
+        return p.put(p.get(a), find(p.get(a)));
+    }
+    
+    public void union(String a, String b) {
+        p.put(b, a);
+    }
+    
+    public void dfs(String c) {
+        for (Object[] o : g.get(c)) {
+            String nextNode = (String) o[0];
+            double nextValue = (double) o[1];
+            
+            if (v.get(nextNode) == 0) {
+                v.put(nextNode, v.get(c) * nextValue);
+                dfs(nextNode);
+            }
+        }
+    }
+    
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {    
+        g = new HashMap<>();
+        v = new HashMap<>();
+        p = new HashMap<>();
+        
+        for (int i = 0; i < equations.size(); i++) {
+            String a = equations.get(i).get(0);
+            String b = equations.get(i).get(1);
+            v.put(a, 0d);
+            v.put(b, 0d);
+            p.put(a, a);
+            p.put(b, b);
+            
+            if (!g.containsKey(a)) {
+                g.put(a, new ArrayList<>());
+            }
+            
+            if (!g.containsKey(b)) {
+                g.put(b, new ArrayList<>());
+            }
+            
+            g.get(a).add(new Object[] {b, 1 / values[i]});
+            g.get(b).add(new Object[] {a, values[i]});
+        }
+        
+        for (int i = 0; i < equations.size(); i++) {
+            String a = equations.get(i).get(0);
+            String b = equations.get(i).get(1);
+            
+            a = find(a);
+            b = find(b);
+            
+            if (!a.equals(b)) {
+                union(a, b);
+            }
+        }
+        
+        for (Map.Entry<String, ArrayList<Object[]>> entry : g.entrySet()) {
+            if (v.get(entry.getKey()) == 0) {
+                v.put(entry.getKey(), 1d);
+                dfs(entry.getKey());
+            }
+        }
+        
+        System.out.println(v);
+        double[] ans = new double[queries.size()];
+        for (int i = 0; i < queries.size(); i++) {
+            String a = queries.get(i).get(0);
+            String b = queries.get(i).get(1);
+            String c = "";
+            String d = "";
+
+            System.out.println(a + " " + b);
+            System.out.println(!p.containsKey(a) || !p.containsKey(b));
+
+            if (!(!p.containsKey(a) || !p.containsKey(b))) {
+                find(a);
+                find(b);
+            }
+            
+            if ((!p.containsKey(a) || !p.containsKey(b)) || !find(a).equals(find(b))) {
+                ans[i] = -1;
+            } else {
+                ans[i] = v.get(a) / v.get(b);
+            }
+        }
+        
+        return ans; 
+    }
+}
