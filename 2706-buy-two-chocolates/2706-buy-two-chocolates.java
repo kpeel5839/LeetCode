a9e7c6033d78c80faaa76faf14100a7753e35ec5
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-05-28 02:29:56 +0900

    Time: 3 ms (20.00%), Space: 43.8 MB (20.00%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        int sum = prices[0] + prices[1];
+        
+        if (sum <= money) {
+            return money - sum;
+        }
+        
+        return money;
+    }
+}
