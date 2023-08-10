933a74a2abf1b33660fc4af746e82e56ab2e6912
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-08-10 21:31:01 +0900

    Time: 0 ms (100.00%), Space: 42.6 MB (98.50%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,10 +1,35 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
+        int l = 0;
+        int r = nums.length - 1;
+        
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            if (nums[m] == target) {
                 return true;
             }
+
+            if (nums[l] == nums[m]) {
+                l++;
+                continue;
+            }
+
+            if (nums[l] < nums[m]) { // 일단, 더 작은 경우는 increase 하고 있는 경우 
+                if (nums[l] <= target && target <= nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if (nums[m] <= target && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;  
+                }
+            }
         }
+
         return false;
     }
 }
