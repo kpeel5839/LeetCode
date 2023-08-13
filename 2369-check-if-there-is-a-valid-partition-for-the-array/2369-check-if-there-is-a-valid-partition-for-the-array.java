a715ea3f2ba4b39d25b7447ce77e5d35c45d8f02
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-08-14 08:42:02 +0900

    Time: 6 ms (92.86%), Space: 56.9 MB (59.18%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,30 +1,25 @@
-class Solution:
-    def validPartition(self, nums: List[int]) -> bool:
-        n = len(nums) 
-
-        # If the array has only one element, then not possible to partition into valid subarrays 
-        if n == 1: 
-            return False 
-
-        # Initialization for the first three values 
-        dp = [True, False, nums[0] == nums[1] if n > 1 else False] 
-
-        for i in range(2, n): 
-            current_dp = False 
-
-            # Check for 2 equal elements 
-            if nums[i] == nums[i-1] and dp[1]: 
-                current_dp = True 
-
-            # Check for 3 equal elements 
-            elif nums[i] == nums[i-1] == nums[i-2] and dp[0]: 
-                current_dp = True 
-
-            # Check for 3 consecutive increasing elements 
-            elif nums[i] - nums[i-1] == 1 and nums[i-1] - nums[i-2] == 1 and dp[0]: 
-                current_dp = True 
-
-            # Move the window forward 
-            dp[0], dp[1], dp[2] = dp[1], dp[2], current_dp 
-
-        return dp[2]
+class Solution {
+    public boolean validPartition(int[] nums) {
+        int n = nums.length;
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        
+        for (int i = 2; i <= n; i++) {
+            int a = nums[i - 1];
+            int b = nums[i - 2];
+            
+            if (a == b) {
+                dp[i] |= dp[i - 2];
+            }
+            
+            if (i != 2) {
+                int c = nums[i - 3];
+                if ((c == b && b == a) || (c == b - 1 && b == a - 1)) {
+                    dp[i] |= dp[i - 3];
+                }
+            } 
+        }
+        
+        return dp[n];
+    }
+}
