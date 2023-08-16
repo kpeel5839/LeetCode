4cc9c47d4992374b375e090b1c2fd636f42b4e02
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-08-16 09:17:45 +0900

    Time: 148 ms (11.70%), Space: 56.6 MB (99.24%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -2,29 +2,24 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         Map<Integer, Integer> m = new HashMap<>();
         PriorityQueue<Integer> q = new PriorityQueue<>((o1, o2) -> o2 - o1);
-        int[] ans = new int[nums.length - (k - 1)];
-        
-        for (int i = 1; i <= nums.length; i++) {
-            q.add(nums[i - 1]);
-            m.put(nums[i - 1], m.getOrDefault(nums[i - 1], 0) + 1);
-            
-            if (k <= i) {
-                while (true) {
-                    int value = q.peek();
-                    
-                    if (m.get(value) == 0) {
-                        q.poll(); 
-                        continue;
-                    }
-                   
-                    ans[i - k] = value;
-                    break;
+        int[] ans = new int[nums.length - k + 1];
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            q.add(nums[i]);
+            m.put(nums[i], m.getOrDefault(nums[i], 0) + 1);
+            count++;
+
+            if (k <= count) {
+                while (m.get(q.peek()) == 0) {
+                    q.poll(); 
                 }
                 
-                m.put(nums[i - k], m.get(nums[i - k]) - 1);
+                ans[i - k + 1] = q.peek();
+                m.put(nums[i - k + 1], m.get(nums[i - k + 1]) - 1);
             }
         }
-        
+
         return ans;
     }
 }
