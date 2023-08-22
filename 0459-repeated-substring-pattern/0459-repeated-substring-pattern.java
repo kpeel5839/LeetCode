0c25704030e76fb530d391dba6e4ec655cddf372
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-08-22 21:03:08 +0900

    Time: 39 ms (63.84%), Space: 44.1 MB (60.99%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,9 +1,37 @@
-class Solution:
-    def repeatedSubstringPattern(self, s: str) -> bool:
-        n = len(s)
-        for i in range(1, n//2 + 1):
-            if n % i == 0:
-                substring = s[:i]
-                if substring * (n // i) == s:
-                    return True
-        return False
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i != 0) {
+                continue;
+            }
+            
+            int count = 0;
+            Set<String> set = new HashSet<>();
+            StringBuilder sb = new StringBuilder();        
+            
+            for (int j = 0; j < n; j++) {
+                count++;
+                sb.append(s.charAt(j));
+                
+                if (count == i) {
+                    set.add(sb.toString());
+                    
+                    if (set.size() == 2) {
+                        break;
+                    }
+                    
+                    sb = new StringBuilder();
+                    count = 0;
+                }
+            }
+            
+            if (set.size() == 1) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
