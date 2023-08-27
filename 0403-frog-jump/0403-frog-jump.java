bbe3ce21907cd1f9e0c46c674cb43e30d1420e86
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-08-27 22:37:05 +0900

    Time: 56 ms (50.50%), Space: 63.8 MB (11.49%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -3,6 +3,7 @@ class Solution {
     public int[][] dp;
     public int[] stones;
     public int[] dx = {-1, 0, 1};
+    public Map<Integer, Integer> m = new HashMap<>();
     public int dfs(int jump, int index) {
         if (index == n - 1) {
             return 1; 
@@ -13,18 +14,11 @@ class Solution {
         }
 
         dp[jump][index] = 0;
-
-        for (int i = index + 1; i < n; i++) {  
-            if (stones[index] + (jump + 1) < stones[i]) {
-                break; 
-            }
-
-            for (int j = 0; j < 3; j++) {
-                int newValue = stones[index] + jump + dx[j];
-                
-                if (newValue == stones[i]) {
-                    dp[jump][index] |= dfs(jump + dx[j], i);
-                }
+        for (int j = 0; j < 3; j++) {
+            int newValue = stones[index] + jump + dx[j];
+            
+            if (m.containsKey(newValue) && index < m.get(newValue)) {
+                dp[jump][index] |= dfs(jump + dx[j], m.get(newValue));
             }
         }
 
@@ -35,6 +29,9 @@ class Solution {
         this.n = stones.length;
         this.stones = stones;
         this.dp = new int[n + 1][n];
+        for (int i = 0; i < n; i++) {
+            m.put(stones[i], i);
+        }
         for (int i = 0; i < dp.length; i++) {
             Arrays.fill(dp[i], -1);
         }
