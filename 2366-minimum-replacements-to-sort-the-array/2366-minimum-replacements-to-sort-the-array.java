08fc427384cb8049cee3d02c6556bb311cd43044
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-09-12 21:25:40 +0900

    Time: 9 ms (8.24%), Space: 55.2 MB (7.10%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.java
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.java
@@ -1,22 +1,19 @@
 class Solution {
-public:
-    long long minimumReplacement(vector<int>& nums) {
-        int n = nums.size();
-        int last = nums[n-1];  // Initialize 'last' with the last element
-        long long ans = 0;     // Initialize the total operations count
+    public long minimumReplacement(int[] nums) {
+        long ans = 0;
+        long value = nums[nums.length - 1];
 
-        // Traverse the array in reverse order
-        for (int i = n - 2; i >= 0; --i) {
-            if (nums[i] > last) {  // If the current element needs replacement
-                int t = nums[i] / last;  // Calculate how many times the element needs to be divided
-                if (nums[i] % last) t++; // If there's a remainder, increment 't'
-                last = nums[i] / t;  // Update 'last' for the next comparison
-                ans += t - 1; // Add (t - 1) to 'ans' for the number of operations
-            } else {
-                last = nums[i]; // Update 'last' without replacement
+        for (int i = nums.length - 2; 0 <= i; i--) {
+            if (nums[i] < value) {
+                value = nums[i];
+                continue;
             }
-        }
-        return ans; // Return the total number of operations
-    }
 
-};
+            long count = (long) Math.ceil((double) nums[i] / (double) value);
+            ans += (count - 1);
+            value = (long) Math.floor((double) nums[i] / (double) count);
+        }
+
+        return ans;
+    }
+}
