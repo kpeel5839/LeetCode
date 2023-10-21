2b5d8d309f756db49d7f005106814aff20bd5a3a
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-10-22 02:02:58 +0900

    Time: 20 ms (60.67%), Space: 61.5 MB (92.57%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.java
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.java
@@ -1,63 +1,39 @@
-
 class Solution {
-    private int[] startNodes;
-    private int[][] graph;
-    private int[] time;
-    private boolean[] visited;
-    private int[] ans;
-
-    private void convert(final int[][] edges, final int n) {
-        final int[] incoming = new int[n], outgoing = new int[n];
-        for(final int[] e : edges) {
-            outgoing[e[0] - 1]++;
-            incoming[e[1] - 1]++;
-        }
-        int startCnt = 0;
-        for(final int i : incoming) {
-            if (i == 0) {
-                startCnt++;
-            }
-        }
-        startNodes = new int[startCnt];
-        for(int sni = 0, i = 0; sni < startNodes.length; i++) {
-            if (incoming[i] == 0) {
-                startNodes[sni++] = i;
-            }
-        }
-        graph = new int[n][];
-        for(int i = 0; i < n; i++) {
-            graph[i] = new int[outgoing[i]];
-        }
-        for(final int[] e : edges) {
-            graph[e[0] - 1][--outgoing[e[0] - 1]] = e[1] - 1;
-        }
-    }
-
-    private int fill(final int node) {
-        if (ans[node] > 0) {
-            return ans[node];
-        }
-
-        int worstPrereq = 0;
-        visited[node] = true;
-        for(final int child : graph[node]) {
-            if (!visited[child]){
-                worstPrereq = Math.max(fill(child), worstPrereq);
-            }
-        }
-        visited[node] = false;
-        return ans[node] = worstPrereq + time[node];
-    }
-
     public int minimumTime(int n, int[][] relations, int[] time) {
-        convert(relations, n);
-        this.time = time;
-        ans = new int[n];
-        visited = new boolean[n];
-        int longest = 0;
-        for(final int node : startNodes) {
-            longest = Math.max(longest, fill(node));
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] entry = new int[n + 1];
+        int[] month = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
         }
-        return longest;
+        
+        for (int i = 0; i < relations.length; i++) {
+            graph.get(relations[i][0]).add(relations[i][1]);
+            entry[relations[i][1]]++;
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            if (entry[i] == 0) {
+                q.add(new int[] {i, 0});
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int[] p = q.poll();
+            ans = Math.max(ans, p[1] + time[p[0] - 1]);
+            
+            for (Integer next : graph.get(p[0])) {
+                month[next] = Math.max(month[next], p[1] + time[p[0] - 1]);
+
+                if (--entry[next] == 0) {
+                    q.add(new int[] {next, month[next]});
+                }
+            }
+        }
+        
+        return ans;
     }
 }
