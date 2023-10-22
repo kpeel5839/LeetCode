ce5719b0e9a7dd8e474c70f8d13a0ce9ef8eb2f8
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-10-22 23:18:41 +0900

    Time: 14 ms (14.18%), Space: 43.3 MB (30.71%) - LeetHub

---
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    public int n;
+    public int l;
+    public int[][] dp;
+    public int MOD = 1_000_000_007;
+
+    public int dfs(int i, int j) {
+        if (i == n) {
+            if (j == 0) {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        
+        dp[i][j] = 0;
+
+        if (j != 0) {
+            dp[i][j] = (dp[i][j] + dfs(i + 1, j - 1)) % MOD;
+        }
+
+        dp[i][j] = (dp[i][j] + dfs(i + 1, j)) % MOD;
+
+        if (j != l - 1) {
+            dp[i][j] = (dp[i][j] + dfs(i + 1, j + 1)) % MOD;
+        }
+
+        return dp[i][j];
+    }
+
+    public int numWays(int steps, int arrLen) {
+        this.n = steps;
+        this.l = Math.min((steps / 2) + 1, arrLen);
+        dp = new int[n][l];
+        System.out.println(n + " " + l);
+        
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        
+        return dfs(0, 0);
+    }
+}
