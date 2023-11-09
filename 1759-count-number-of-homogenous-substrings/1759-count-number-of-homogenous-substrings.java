d6549c938c6666da36a90f06b671b70a8258f241
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-11-09 20:42:36 +0900

    Time: 8 ms (87.60%), Space: 44.4 MB (20.00%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.java
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.java
@@ -1,18 +1,17 @@
 class Solution {
     public int countHomogenous(String s) {
+        char[] c = s.toCharArray();
         char pre = '1';
         int count = 0;
         int ans = 0;
         final int MOD = 1_000_000_007;
         
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            
-            if (pre == c) {
+            if (pre == c[i]) {
                 count++;
             } else {
                 count = 1;
-                pre = c;
+                pre = c[i];
             }
             
             ans = (ans + count) % MOD;
