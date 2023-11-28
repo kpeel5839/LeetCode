40c85fa681066c7fa1f9f42a04ddbbbbb6f3ddf6
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-11-29 04:07:33 +0900

    Time: 61 ms (48.84%), Space: 58.1 MB (72.09%) - LeetHub

--- 2945-find-maximum-non-decreasing-array-length/2945-find-maximum-non-decreasing-array-length.java
+++ 2945-find-maximum-non-decreasing-array-length/2945-find-maximum-non-decreasing-array-length.java
@@ -1,37 +1,37 @@
 public class Solution {
     public int findMaximumLength(int[] nums) {
         int n = nums.length;
-
         long[] prefixSum = new long[n + 1];
+        
         for (int i = 0; i < n; i++) {
-            prefixSum[i + 1] = (long) nums[i] + prefixSum[i];
+            prefixSum[i + 1] = nums[i] + prefixSum[i];
         }
-
+        
         int[] range = new int[n + 1];
         int[] dp = new int[n + 1];
-
-        for (int i = 1; i <= n; ++i) {
+        
+        for (int i = 1; i <= n; i++) {
             range[i] = Math.max(range[i], range[i - 1]);
             dp[i] = dp[range[i]] + 1;
-
+            
             int l = i;
-            int r = n; 
+            int r = n;
             int ans = 0;
-
+            
             while (l <= r) {
-                int mid = (l + r) / 2;
-
-                if (prefixSum[mid] - prefixSum[i] < prefixSum[i] - prefixSum[range[i]]) {
-                    l = mid + 1;
+                int m = (l + r) / 2;
+                
+                if (prefixSum[m] - prefixSum[i] < prefixSum[i] - prefixSum[range[i]]) {
+                    l = m + 1;
                 } else {
-                    ans = mid;
-                    r = mid - 1;
+                    ans = m;
+                    r = m - 1;
                 }
             }
-
+            
             range[ans] = i;
         }
-
+        
         return dp[n];
     }
 }
