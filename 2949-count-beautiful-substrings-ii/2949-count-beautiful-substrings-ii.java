215d90a414e7ce58b2c9cd8961c5d632c0258fcd
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-11-28 23:27:17 +0900

    Time: 39 ms (81.25%), Space: 46.1 MB (46.09%) - LeetHub

--- 2949-count-beautiful-substrings-ii/2949-count-beautiful-substrings-ii.java
+++ 2949-count-beautiful-substrings-ii/2949-count-beautiful-substrings-ii.java
@@ -1,28 +1,37 @@
 public class Solution {
     public int beautifulSubstrings(String s, int k) {
-        Map<Integer, List<Integer>> grps = new HashMap<>();
-        grps.put(0, new ArrayList<>());
-        grps.get(0).add(0);
+        Map<Integer, List<Integer>> m = new HashMap<>();
+        Set<Character> set = new HashSet<>(Set.of('a', 'i', 'e', 'o', 'u'));
+        int sum = 0;
+        m.put(0, new ArrayList<>());
+        m.get(0).add(0);
 
-        String vowels = "aeiou";
-        for (int i = 0, presum = 0; i < s.length(); i++) {
-            presum += vowels.indexOf(s.charAt(i)) != -1 ? 1 : -1;
-            grps.computeIfAbsent(presum, key -> new ArrayList<>()).add(i + 1);
+        for (int i = 0; i < s.length(); i++) {
+            if (set.contains(s.charAt(i))) {
+                sum++;
+            } else {
+                sum--;
+            }
+            
+            m.computeIfAbsent(sum, ignored -> new ArrayList<>()).add(i + 1);
         }
 
-        int x;
-        for (x = 1; x <= k && x * x % k != 0; x++);
-        x *= 2;
-
-        long ans = 0;
-        for (Map.Entry<Integer, List<Integer>> entry : grps.entrySet()) {
-            List<Integer> grp = entry.getValue();
-            int[] mod = new int[x];
-            for (int i : grp) {
-                ans += mod[i % x]++;
+        int newK = 0;
+        for (int i = 1; i <= k; i++) {
+            if ((i * i) % k == 0) {
+                newK = i * 2;
+                break;
             }
         }
 
-        return (int) ans;
+        int ans = 0;
+        for (List<Integer> list : m.values()) {
+            int[] mod = new int[newK];
+            for (int value : list) {
+                ans += mod[value % newK]++;
+            }
+        }
+
+        return ans;
     }
 }
