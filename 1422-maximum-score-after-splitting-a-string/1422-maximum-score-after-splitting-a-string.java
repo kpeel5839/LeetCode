7d58b8231443c28108cefa1df34ffd7ad2080bd8
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-12-22 11:06:20 +0900

    Time: 2 ms (56.44%), Space: 41.4 MB (32.89%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxScore(String s) {
+        int[][] prefixSum = new int[2][s.length() + 1];
+        
+        for (int i = 1; i <= s.length(); i++) {
+            int number = s.charAt(i - 1) - '0';
+            prefixSum[0][i] = prefixSum[0][i - 1] + (number == 0 ? 1 : 0);
+            prefixSum[1][i] = prefixSum[1][i - 1] + (number == 1 ? 1 : 0);
+        }
+        
+        int answer = 0;
+        for (int i = 1; i < s.length(); i++) {
+            answer = Math.max(answer, prefixSum[0][i] + (prefixSum[1][s.length()] - prefixSum[1][i]));
+        }
+        return answer;
+    }
+}
