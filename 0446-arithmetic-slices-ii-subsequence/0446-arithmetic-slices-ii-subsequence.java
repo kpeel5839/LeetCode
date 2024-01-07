87c78fc39f3d66d64a50a64a890c00dd97a5d6da
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-01-07 13:07:56 +0900

    Time: 164 ms (55.23%), Space: 94.7 MB (25.58%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.java
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.java
@@ -1,31 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
-        int size = nums.length;
-        Map<Integer, Integer>[] frequencyOfDiff = new Map[size];
-        
-        for (int i = 0; i < size; i++) {
-            frequencyOfDiff[i] = new HashMap<>();
-        } 
-
         int answer = 0;
-
-        for (int i = 0; i < size; i++) {
+        int n = nums.length;
+        Map<Long, Integer>[] m = new HashMap[n];
+        m[0] = new HashMap<>();
+        
+        for (int i = 1; i < n; i++) {
+            m[i] = new HashMap<>();
+            
             for (int j = 0; j < i; j++) {
-                long commonDiff = (long) nums[i] - (long) nums[j];
-                
-                if (commonDiff < Integer.MIN_VALUE || commonDiff > Integer.MAX_VALUE) {
-                    continue;   
-                }
-
-                int diff = (int) commonDiff;
-                int previousFrequencyOfDiff = frequencyOfDiff[j].getOrDefault(diff, 0);
-                answer += previousFrequencyOfDiff;
-                frequencyOfDiff[i].put(diff, frequencyOfDiff[i].getOrDefault(diff, 0) + previousFrequencyOfDiff + 1);
-            }        
+                long diff = (long) nums[i] - (long) nums[j];
+                int count = m[j].getOrDefault(diff, 0);
+                answer += count;
+                m[i].put(diff, m[i].getOrDefault(diff, 0) + count + 1);
+            }
         }
-
+        
         return answer;
     }
 }
