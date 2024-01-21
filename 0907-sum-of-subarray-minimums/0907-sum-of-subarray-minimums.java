ee00b5be7c29863a4fba8fd2a72f55105be7f833
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-01-21 16:59:41 +0900

    Time: 30 ms (84.32%), Space: 49.6 MB (41.32%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,47 +1,28 @@
 import java.util.*;
 
 class Solution {
-    final int MOD = 1_000_000_007;
 
     public int sumSubarrayMins(int[] arr) {
-        /*
-        어떻게 해야할까?
-        이 문제의 핵심은 boundary 를 구해서, 그 범위를 이용하여 계산하는 것이 핵심인 문제 
-        */    
-        Stack<int[]> monoStack = new Stack<>();
-        long answer = 0;
-        int arrSize = arr.length;    
-        long[] left = new long[arrSize];
-        long[] right = new long[arrSize];
+        Stack<int[]> s = new Stack<>(); // {count, value}
+        int MOD = 1_000_000_007;
+        int stackSum = 0;
+        int answer = 0;
         
-        for (int i = 0; i < arrSize; i++) {
-            int countOfsmallerThanThisIndex = 1; 
+        for (int i = 0; i < arr.length; i++) {
+            int productCount = 1;
             
-            while (!monoStack.empty() && monoStack.peek()[0] > arr[i]) { // 여기는 더 작은 것만 따지는 이유는 모두 같은 경우를 생각해보면 쉽다.
-                countOfsmallerThanThisIndex += monoStack.pop()[1];
+            while (!s.isEmpty() && s.peek()[1] > arr[i]) {
+                int[] v = s.pop();
+                stackSum -= (v[0] * v[1]);
+                productCount += v[0];
             }
-
-            monoStack.push(new int[] {arr[i], countOfsmallerThanThisIndex});
-            left[i] = countOfsmallerThanThisIndex;
-        }
-
-        monoStack.clear();    
-
-        for (int i = arrSize - 1; i != -1; i--) {
-                int countOfsmallerThanThisIndex = 1;
             
-            while (!monoStack.empty() && monoStack.peek()[0] >= arr[i]) {
-                countOfsmallerThanThisIndex += monoStack.pop()[1];            
-            }
-
-            monoStack.push(new int[] {arr[i], countOfsmallerThanThisIndex});
-            right[i] = countOfsmallerThanThisIndex;        
+            stackSum += (arr[i] * productCount);
+            s.push(new int[] {productCount, arr[i]});
+            answer = (answer + stackSum) % MOD;
         }
-
-        for (int i = 0; i < arrSize; i++) {
-            answer = (answer + arr[i] * left[i] * right[i]) % MOD;
-        }
-
-        return (int) answer;
+        
+        return answer;
     }
+    
 }
