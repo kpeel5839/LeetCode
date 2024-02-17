bacfc3a52211d5236b79f103ab30e8b07bac0d52
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-02-18 01:11:09 +0900

    Time: 454 ms (40.00%), Space: 126.6 MB (60.00%) - LeetHub

---
+++ 3041-maximize-consecutive-elements-in-an-array-after-modification/3041-maximize-consecutive-elements-in-an-array-after-modification.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+  typedef vector<int> vi;
+  typedef vector<vi> vii;
+  typedef vector<vii> viii;
+  int maxSelectedElements(vector<int>& nums) {
+    sort(nums.begin(),nums.end());
+    int n=nums.size();
+    viii dp(2,vii(2,vi(nums.size(),1)));
+    map<int,int>m;
+    int answer=1;
+    for(int i=0;i<n;i++){
+      if(m.find(nums[i])!=m.end()){
+        int index=m[nums[i]];
+        dp[0][1][i]=max(dp[0][1][i],dp[0][0][index]+1);
+        dp[1][0][i]=max(dp[1][0][i],dp[1][1][index]+1);
+      }
+      if(m.find(nums[i]-1)!=m.end()){
+        int index=m[nums[i]-1];
+        dp[0][0][i]=max(dp[0][0][i],dp[0][0][index]+1);
+        dp[0][1][i]=max(dp[0][1][i],dp[0][1][index]+1);
+      }
+      if(m.find(nums[i]-2)!=m.end()){
+        int index=m[nums[i]-2];
+        dp[0][0][i]=max(dp[0][0][i],dp[0][1][index]+1);
+      }
+      for(int j=0;j<2;j++){
+        for(int c=0;c<2;c++){
+          answer=max(answer,dp[j][c][i]);
+        }
+      }
+      m[nums[i]]=i;
+    }
+    return answer;
+  }
+};
