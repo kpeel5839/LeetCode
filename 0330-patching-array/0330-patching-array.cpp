710a60d1ab0269639c2b0146d9811e36d2e797e8
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-06-16 18:09:45 +0900

    Time: 0 ms (100%), Space: 13.9 MB (12.3%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     typedef long long ll;
     int minPatches(vector<int>& nums, int n) {
-        ll sum=1;
+        ll sum=0;
         int size=nums.size();
         sort(nums.begin(),nums.end());
         int answer=0;
         int pointer=0;
-        while(sum<=n){
-            if(pointer!=size&&nums[pointer]<=sum){
+        while(sum<n){
+            if(pointer!=size&&nums[pointer]<=sum+1){
                 sum+=nums[pointer++];
                 continue;
             }
             answer++;
-            sum+=sum;
+            sum+=sum+1;
         }
         return answer;
     }
