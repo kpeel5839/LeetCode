6264322e2e5ed25466ff946bd3af64e4620640fd
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-06-17 19:42:51 +0900

    Time: 206 ms (5%), Space: 78.5 MB (18.45%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
+    typedef long long ll;
     bool judgeSquareSum(int c) {
-        for (int divisor = 2; divisor * divisor <= c; divisor++) {
-            if (c % divisor == 0) {
-                int exponentCount = 0;
-                while (c % divisor == 0) {
-                    exponentCount++;
-                    c /= divisor;
-                }
-                if (divisor % 4 == 3 && exponentCount % 2 != 0) {
-                    return false;
-                }
+        // for(int i=2;i*i<=c;i++){
+        //     if(c%i==0){
+        //         int cnt=0;
+        //         while(c%i==0){
+        //             cnt++;
+        //             c/=i;
+        //         }
+        //         if(i%4==3&&cnt%2!=0){
+        //             return false;
+        //         }
+        //     }
+        // } // 4k+3 형태의 소인수가 짝수개 있으면 끝나는 것
+        // return c%4!=3; // 이미 짝수개 다 맞춰놨는데 c%4==3 추가되면 홀 수 되니까 false
+        if(c==0){
+            return true;
+        }
+        unordered_set<ll>s;
+        for(ll i=0;i*i<=c;i++){
+            s.insert(i*i);
+        }
+        for(ll i=1;i*i<=c;i++){
+            ll diff=c-i*i;
+            if(s.find(diff)!=s.end()){
+                return true;
             }
         }
-        return c % 4 != 3;;
+        return false;
     }
 };
