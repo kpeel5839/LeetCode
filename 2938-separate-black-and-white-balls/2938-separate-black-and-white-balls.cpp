61eb75444ee930fe2b426aaf9159e377d747fb6a
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-10-15 22:23:04 +0900

    Time: 35 ms (43.16%), Space: 15.7 MB (45.68%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    typedef long long ll;
+    typedef vector<ll> vl;
+    typedef vector<vl> vll;
+
+    long long minimumSteps(string s) {
+        ll answer=0;
+        int n=s.size();
+        int goal=n-1;
+        for(int i=n-1;0<=i;i--){
+            if(s[i]=='1'){
+                answer+=(goal-i);
+                goal--;
+            }
+        }
+        return answer;
+    }
+};
