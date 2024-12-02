2a455bc4c1c9048ea6ca3121084cec9754777ec9
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-12-03 07:09:52 +0900

    Time: 0 ms (100%), Space: 50.7 MB (45.24%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -0,0 +1,9 @@
+var checkIfExist = function(arr) {
+    let set = new Set()
+    for(let i=0;i<arr.length;i++){
+        if(set.has(arr[i]*2))return true
+        if(set.has(arr[i]/2))return true
+        set.add(arr[i])
+    }
+    return false
+};
